81c39f27b231b2ea4853217dcebaf20bf8d53a22
safaaal1 <93180629+safaaal1@users.noreply.github.com>
2023-06-05 08:24:52 +0300

    test

--- client/src/unitTest.js
+++ client/src/unitTest.js
@@ -1,90 +1,53 @@
 import React from "react";
-import { render, fireEvent, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import Goals from './components/setgoals';
-import Contact from "./components/Contact/contact";
-import axios from 'axios';
+import { shallow } from "enzyme";
+import axios from "axios";
+import GoalsDisplay from "./GoalsDisplay";
 
-jest.mock("@emailjs/browser", () => ({
-  sendForm: jest.fn(() => Promise.resolve(true))
-}));
+jest.mock("axios");
 
-jest.mock('axios');
-
-describe("Contact", () => {
-  test("renders Contact component", () => {
-    render(<Contact />);
-    
-    // test header
-    expect(screen.getByText(/Contact Us/i)).toBeInTheDocument();
-    expect(screen.getByText(/If you have any questions or comments, please fill out the form below and/i)).toBeInTheDocument();
-    expect(screen.getByText(/we will get back to you as soon as possible./i)).toBeInTheDocument();
-
-    // test input fields
-    expect(screen.getByLabelText(/Name:/i)).toBeInTheDocument();
-    expect(screen.getByLabelText(/Email:/i)).toBeInTheDocument();
-    expect(screen.getByLabelText(/Message:/i)).toBeInTheDocument();
+describe("GoalsDisplay", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  test("updates on input change", () => {
-    render(<Contact />);
+  test("fetchGoals should fetch goals data and update state", async () => {
+    const userEmail = 'salaqe@ac.sce.ac.il';
+    const goalsData = {
+      currentWeight: 70,
+      currentLength: 180,
+      goalWeight: 65,
+      muscleGain: 5,
+      exerciseDays: 4,
+    };
+    axios.get.mockResolvedValueOnce({ data: { goals: goalsData } });
 
-    const nameInput = screen.getByLabelText(/Name:/i);
-    const emailInput = screen.getByLabelText(/Email:/i);
-    const messageInput = screen.getByLabelText(/Message:/i);
+    const wrapper = shallow(<GoalsDisplay />);
+    wrapper.setState({ userEmail });
 
-    fireEvent.change(nameInput, { target: { value: 'John' } });
-    fireEvent.change(emailInput, { target: { value: 'john@example.com' } });
-    fireEvent.change(messageInput, { target: { value: 'Hello!' } });
+    await wrapper.instance().fetchGoals();
 
-    expect(nameInput.value).toBe('John');
-    expect(emailInput.value).toBe('john@example.com');
-    expect(messageInput.value).toBe('Hello!');
-  });
-
-  test("calls emailjs.sendForm on form submit", async () => {
-    render(<Contact />);
-
-    userEvent.type(screen.getByLabelText(/Name:/i), "John");
-    userEvent.type(screen.getByLabelText(/Email:/i), "john@example.com");
-    userEvent.type(screen.getByLabelText(/Message:/i), "Hello!");
-
-    const form = screen.getByRole("form");
-    fireEvent.submit(form);
-
-    expect(emailjs.sendForm).toHaveBeenCalled();
-  });
-});
-
-describe('<Goals />', () => {
-  it('should change state on input change and submit form', async () => {
-    const { getByLabelText, getByRole } = render(<Goals />);
-    localStorage.setItem('email', 'test@test.com');
-
-    // Simulate filling in the form
-    fireEvent.change(getByLabelText('Current Weight:'), { target: { value: '150' } });
-    fireEvent.change(getByLabelText('Goal Weight:'), { target: { value: '130' } });
-    fireEvent.change(getByLabelText('muscle gain:'), { target: { value: '20' } });
-    fireEvent.change(getByLabelText('Exercise days:'), { target: { value: '5' } });
-
-    // Simulate form submission
-    fireEvent.click(getByRole('button'));
-
-    // Wait for the post request to complete
-    await waitFor(() => expect(axios.post).toHaveBeenCalledTimes(1));
-
-    // Verify the axios.post was called with correct parameters
-    expect(axios.post).toHaveBeenCalledWith(
-      "http://localhost:4000/api/user/setgoals",
+    expect(axios.get).toHaveBeenCalledWith(
+      "http://localhost:4000/api/user/getgoals",
       {
-        email: 'test@test.com',
-        currentWeight: '150',
-        goalWeight: '130',
-        muscleGain: '20',
-        exerciseDays: '5',
+        params: {
+          email: userEmail,
+        },
       }
     );
+    expect(wrapper.state("goals")).toEqual(goalsData);
+  });
+
+  test("calculateBMI should return the correct BMI value", () => {
+    const wrapper = shallow(<GoalsDisplay />);
+    const instance = wrapper.instance();
+
+    const weight = 70;
+    const height = 180;
+    const expectedBMI = "21.60";
+
+    const result = instance.calculateBMI(weight, height);
+
+    expect(result).toBe(expectedBMI);
   });
 });
-
 
